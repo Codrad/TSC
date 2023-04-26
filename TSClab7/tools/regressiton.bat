cfc6d49ec45879073ca172048d278a668590eaed
Radulescu Codrin-Alin <ralin800@gmail.com>
2023-04-26 20:44:38 +0300

    TSClab7

---
+++ TSClab7/tools/regressiton.bat
@@ -0,0 +1,7 @@
+::========================================================================================
+call ../tools/run_test-ii.bat c
+call ../tools/run_test-ri.bat c
+call ../tools/run_test-ir.bat c
+call ../tools/run_test-rr.bat c
+pause
+::make multiple run_test.bat with dfferent cases. Use -c(terminal). Add more variables to 
